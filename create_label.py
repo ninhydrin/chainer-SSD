7b5c8f714e40e42d247dfe199e560d7e0e4037ec
ninhydrin <yokoshima@yokoshimafu-no-MacBook-Air.local>
2016-12-01 10:06:35 +0900

    change encoder

--- create_label.py
+++ create_label.py
@@ -1,8 +1,12 @@
+import pickle
+
 import numpy as np
+
 import bbox
 import ssd_net
 
 model = ssd_net.SSD()
+data = pickle.load(open("test_voc2007.pkl", "rb"))
 
 
 class TrainMaker:
@@ -10,30 +14,31 @@ class TrainMaker:
         self.prior = prior
         self.datas = datas
         self.insize = insize
+        self.prior_num = self.prior.shape[0]
 
     def make_sample(self, data):
         path, size, BBs = data
         h, w = size
         label = BBs[:, 0]
-        prior_num = self.prior.shape[0]
+
         BB = BBs[:, 1:].copy()
         BB[:, (0, 2)] *= self.insize / w
         BB[:, (1, 3)] *= self.insize/h
-
-        loc_mask = np.zeros([prior_num, 4])
-        conf_mask = np.zeros([prior_num, 21])
+        loc_mask = np.zeros([self.prior_num, 4])
+        conf_mask = np.zeros([self.prior_num, 21])
 
         overlap = bbox.bbox_overlaps2(self.prior[:, 0] * self.insize, BB)
+
         positions = np.array(np.where(overlap > 0.5))
         conf_mask[positions[0]] = 1
         loc_mask[positions[0]] = 1
 
         conf_t = overlap.argmax(1)
-        loc_t = np.zeros([prior_num, 4])
-        loc_t = np.array([self.encoder(BB[conf_t[i]], self.prior[i])
+        loc_t = np.zeros([self.prior_num, 4])
+        loc_t = np.array([self.encoder(BB[conf_t[i]] / self.insize, self.prior[i])
                           if loc_mask[i][0] else [0, 0, 0, 0]
-                          for i in range(prior_num)])
-        conf_t = [label[conf_t[i]] for i in range(prior_num)]
+                          for i in range(self.prior_num)])
+        conf_t = [label[conf_t[i]] for i in range(self.prior_num)]
         conf_t *= conf_mask[:, 0]
         conf_t += conf_mask[:, 0]
         return ([loc_mask, conf_mask, loc_t, conf_t])
@@ -41,7 +46,7 @@ class TrainMaker:
     def encoder(self, bbox, prior):
         prior_bbox = prior[0]
         prior_variance = prior[1]
-        bbox_data = np.array([0] * 4, dtype=np.float32)
+        encode_bbox = np.array([0] * 4, dtype=np.float32)
         prior_width = prior_bbox[2] - prior_bbox[0]
         prior_height = prior_bbox[3] - prior_bbox[1]
         prior_center_x = (prior_bbox[0] + prior_bbox[2]) / 2.
@@ -49,11 +54,11 @@ class TrainMaker:
         bbox_width = bbox[2] - bbox[0]
         bbox_height = bbox[3] - bbox[1]
         if bbox_width <= 0 or bbox_height <= 0:
-            return bbox_data
+            return encode_bbox
         bbox_center_x = (bbox[0] + bbox[2]) / 2.
         bbox_center_y = (bbox[1] + bbox[3]) / 2.
-        bbox_data[0] = (bbox_center_x - prior_center_x) / prior_width / prior_variance[0]
-        bbox_data[1] = (bbox_center_y - prior_center_y) / prior_height / prior_variance[1]
-        bbox_data[2] = np.log(bbox_width / prior_width) / prior_variance[2]
-        bbox_data[3] = np.log(bbox_height / prior_height) / prior_variance[3]
-        return bbox_data
+        encode_bbox[0] = (bbox_center_x - prior_center_x) / prior_width / prior_variance[0]
+        encode_bbox[1] = (bbox_center_y - prior_center_y) / prior_height / prior_variance[1]
+        encode_bbox[2] = np.log(bbox_width / prior_width) / prior_variance[2]
+        encode_bbox[3] = np.log(bbox_height / prior_height) / prior_variance[3]
+        return encode_bbox
